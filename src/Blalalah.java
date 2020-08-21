e271714d80e54be88ba61dfd121c7e852c41996e
zybdozzzzz <zybdoz@gmail.com>
2020-08-21 18:39:11 -0500

    My first commit ver 0.1 Added a sum function.

---
+++ src/Blalalah.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Nothing in there
+ * Don't read
+ * I told you do not read
+ */
+
+public class Blalalah {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers below");
+        Scanner number = new Scanner(System.in);
+        double a = Double.parseDouble(number.nextLine()); // take in a number from user
+        double b = Double.parseDouble(number.nextLine());
+
+        double result = sum(a, b);
+        System.out.println(result);
+    }
+
+    public static double sum(double num1, double num2) {
+        double result = num1 + num2;
+        return result;
+    }
+}
